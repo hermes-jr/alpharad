0099d7fcff76b58714b8861f60014d743ad0b414
Mikhail Antonov <hermes@cyllene.net>
2020-11-18 22:15:39 +0300

    Added output file configuration option

--- qa/plot_data.py
+++ qa/plot_data.py
@@ -34,7 +34,7 @@ from matplotlib.ticker import FormatStrFormatter
 try:
     from scipy import stats
 except ImportError:
-    print("scipy.stats not imported, skipping data stats calculation")
+    print('scipy.stats not imported, skipping data stats calculation')
     stats = None
 
 import matplotlib.pyplot as plt
@@ -42,6 +42,8 @@ import matplotlib.pyplot as plt
 settings = None
 xs, ys, as_bytes = [], [], []
 width, height = 640, 480
+x_count = {}
+y_count = {}
 title = 'alpharad plot'
 
 
@@ -60,19 +62,21 @@ def read_data():
             coordinates = line.split(':')
             x = int(coordinates[0])
             xs.append(x)
+            x_count[x] = x_count.get(x, 0) + 1
             y = int(coordinates[1])
             ys.append(y)
-    v_print("Points read: {}".format(len(xs)))
+            y_count[y] = y_count.get(y, 0) + 1
+    v_print('Points read: {}'.format(len(xs)))
 
     with settings.data as f:
         byte = f.read(1)
         while byte:
-            as_bytes.append(int.from_bytes(byte, "big", signed=False))
+            as_bytes.append(int.from_bytes(byte, 'big', signed=False))
             byte = f.read(1)
 
-    v_print("Bytes read: {}".format(len(as_bytes)))
+    v_print('Bytes read: {}'.format(len(as_bytes)))
 
-    title = "{:,} flashes {:,} bytes [{}, {}]".format(
+    title = '{:,} flashes {:,} bytes [{}, {}]'.format(
         len(xs), len(as_bytes), os.path.basename(settings.points.name), os.path.basename(settings.data.name)
     )
 
@@ -86,6 +90,8 @@ def init_arg_parser():
     ap.add_argument('-p', '--points', default='../points.log', metavar='POINTS_LOG',
                     type=argparse.FileType('r'),
                     help='read hit coordinates from POINTS_LOG')
+    ap.add_argument('-o', '--out', default=None, metavar='OUT_FILE',
+                    help='write output to OUT_FILE')
     ap.add_argument('--stats', action='store_true',
                     help='calculate and display data statistics after processing')
     ap.add_argument('--segments', action='store_true',
@@ -108,11 +114,11 @@ def plot():
     gs01 = gs0[1].subgridspec(3, 1)
 
     fig.add_subplot(gs00[0, 0])
-    plt.title("Hits - histogram")
+    plt.title('Hits - histogram')
     plt.hist2d(xs, ys, bins=[(max(xs) - min(xs)) // 4, (max(ys) - min(ys)) // 4], density=True)
 
     fig.add_subplot(gs00[1, 0])
-    plt.title("Hits - small markers")
+    plt.title('Hits - small markers')
     plt.scatter(xs, ys, marker=',', s=1, alpha=0.33, edgecolors='none', snap=False, lw=1)
 
     if settings.segments:
@@ -129,38 +135,68 @@ def plot():
             plt.gca().add_patch(rect)
 
     fig.add_subplot(gs01[0, 0])
-    plt.title("Xs distribution")
+    plt.title('Xs distribution')
     plt.hist(xs, bins=width, snap=False, aa=False)
     plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%6d'))
 
     fig.add_subplot(gs01[1, 0])
-    plt.title("Ys distribution")
+    plt.title('Ys distribution')
     plt.hist(ys, bins=height, snap=False, aa=False)
     plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%6d'))
 
     fig.add_subplot(gs01[2, 0])
-    plt.title("Bytes distribution")
+    plt.title('Bytes distribution')
     plt.hist(as_bytes, bins=256, snap=False, aa=False)
     plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%6d'))
 
     plt.tight_layout()
 
     if settings.window:
-        v_print("Opening plot window")
+        v_print('Opening plot window')
         plt.show()
     else:
-        plt.savefig('plot_{:08d}.png'.format(len(as_bytes)))
+        if settings.out:
+            v_print('Saving to {}'.format(settings.out))
+            plt.savefig(settings.out)
+        else:
+            file_name = 'plot_{:08d}.png'.format(len(as_bytes))
+            v_print('Saving to {}'.format(file_name))
+            plt.savefig(file_name)
 
 
-if __name__ == "__main__":
+def print_frequencies():
+    """
+    Print the number of occurrences of each coordinate to detect black pixels and hot pixels
+    """
+    x_count_ordered = {k: v for k, v in sorted(x_count.items(), key=lambda i: i[1], reverse=True)}
+    y_count_ordered = {k: v for k, v in sorted(y_count.items(), key=lambda i: i[1], reverse=True)}
+
+    if settings.verbose:
+        # Dump everything
+        print('X frequencies:\n{}'.format(x_count_ordered))
+        print('Y frequencies:\n{}'.format(y_count_ordered))
+    else:
+        # A "quiet" version of the above, prints only M first and last items
+        margin = 5
+        print('X count: {} ... {}'.format(
+            {i: x_count_ordered[i] for i in list(x_count_ordered)[:margin]},
+            {i: x_count_ordered[i] for i in list(x_count_ordered)[-margin:]}))
+        print('Y count: {} ... {}'.format(
+            {i: y_count_ordered[i] for i in list(y_count_ordered)[:margin]},
+            {i: y_count_ordered[i] for i in list(y_count_ordered)[-margin:]}))
+
+
+if __name__ == '__main__':
     settings = init_arg_parser().parse_args()
 
     read_data()
 
-    if stats and settings.stats:
-        print("x stats: {}\ny stats: {}".format(
-            stats.describe(xs),
-            stats.describe(ys)))
-        print("byte stats: {}".format(stats.describe(as_bytes)))
+    if settings.stats:
+        if stats:
+            print('x stats: {}\ny stats: {}'.format(
+                stats.describe(xs),
+                stats.describe(ys)))
+            print('byte stats: {}'.format(stats.describe(as_bytes)))
+        print_frequencies()
 
     plot()
